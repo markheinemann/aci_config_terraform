9c73334dc1abffb2637a9b1d700c7f78ec53b338
mark <mark@cc.com>
2023-11-30 15:02:42 +0000

    add vlan pools config

--- main2.tf
+++ main2.tf
@@ -199,7 +199,7 @@ resource "aci_vlan_pool" "vlan_pool" {
 resource "aci_ranges" "range" {
   for_each = { for t in local.yaml_vlan_pool.vlan_pool: t.vlan_pool => t }
   #vlan_pool_dn  = "uni/infra/vlanns-[mark_pool]-static"
-  vlan_pool_dn  = "uni/infra/vlanns-$each.value.vlan_pool_name}-static"
+  vlan_pool_dn  = "uni/infra/vlanns-${each.value.vlan_pool_name}-static"
   description   = "From Terraform"
   from          = each.value.range_from
   to            = each.value.range_to
