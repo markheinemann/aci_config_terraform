15ffe6bdcd70a9ec7d10e0a074e031fefceb0738
mark <mark@cc.com>
2023-12-05 21:12:12 +0000

    enable arp flooding

--- main2.tf
+++ main2.tf
@@ -62,6 +62,7 @@ resource "aci_bridge_domain" "bridge_domains" {
   name        = each.value.bd
   description = each.value.bd_description
   arp_flood   = "yes"
+  unk_mac_ucast_act = "flood"
   relation_fv_rs_ctx = "uni/tn-${each.value.bd_tenant_name}/ctx-${each.value.bd_vrf}"
   #relation_fv_rs_ctx = "marks_vrf"
 
